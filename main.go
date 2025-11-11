c1fce5036a7395a0bcae0971f0f92c4b2ca4d3e8
mohdanas96 <mohammedanasdk963@gmail.com>
2025-11-11 15:29:30 +0530

    Implement db connection and create db schema, included sqlc, goose

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/mohdanas96/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	dbQ            *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbUrl := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		fmt.Printf("Cannot open connection to DB: %v", err)
+		return
+	}
+
+	dbQueries := database.New(db)
+
 	filepathRoot := "."
 	port := ":8080"
 
-	apiConfig := apiConfig{fileServerHits: atomic.Int32{}}
+	apiConfig := apiConfig{fileServerHits: atomic.Int32{}, dbQ: dbQueries}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiConfig.middlewareMetricsInc((http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))))
